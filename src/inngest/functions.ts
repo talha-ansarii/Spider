2725cedb23e427122aa989ef0bd9406002adc4cf
talha-ansarii <talhaansari1696@gmail.com>
2025-08-22 18:18:46 +0530

    Integrates code interpreter and sandbox creation
    Adds code interpreter dependency and logic for creating and retrieving sandbox environments.
    Enables functions to generate sandbox URLs for dynamic usage, supporting extensibility for code execution or isolation features.

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,21 +1,33 @@
 import { inngest } from "./client";
 
 import { createAgent, gemini } from "@inngest/agent-kit";
-
-
+import {Sandbox} from "@e2b/code-interpreter"
+import { getSandbox } from "./utils";
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event}) => {
-    const summarizer = createAgent({
-      name: "Summarizer",
-      system:
-        "You are a Summarizer expert. Summarize the text in 1 word.",
-      model: gemini({model: "gemini-2.0-flash"}),
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("spider-test-2");
+      return sandbox.sandboxId;
     });
 
-    const { output } = await summarizer.run(`Summarize the following text: ${event.data.input}`);
+    const summarizer = createAgent({
+      name: "Summarizer",
+      system: "You are a Summarizer expert. Summarize the text in 1 word.",
+      model: gemini({ model: "gemini-2.0-flash" }),
+    });
 
-    return {  output };
+    const { output } = await summarizer.run(
+      `Summarize the following text: ${event.data.input}`
+    );
+
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    });
+
+    return { output, sandboxUrl };
   }
 );
