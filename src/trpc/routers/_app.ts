22a2ccdf9c83f589629e5445400a010b9ef4814b
talha-ansarii <talhaansari1696@gmail.com>
2025-08-22 15:38:48 +0530

    Integrates Inngest for async job handling and UI feedback
    Adds Inngest workflow support to enable background job invocation from the UI, providing user feedback via toasts. Refactors UI logic to trigger jobs client-side and updates dependencies to support event-driven processing.

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,6 +1,23 @@
 import { z } from "zod";
 import { baseProcedure, createTRPCRouter } from "../init";
+import { inngest } from "@/inngest/client";
 export const appRouter = createTRPCRouter({
+
+  invoke : baseProcedure
+  .input(
+    z.object({
+      text: z.string(),
+    })
+  ).mutation(async (opts) => {
+    const { text } = opts.input;
+    await inngest.send({
+      name: "test/hello.world",
+      data: {
+        email: text,
+      },
+    });
+    
+  }),
   hello: baseProcedure
     .input(
       z.object({
